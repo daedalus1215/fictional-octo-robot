c3a5f7644277a649634ab605592c923ee90a3961
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-04-21 11:59:19 -0400

    Make it so elasticIP is accessible via port 80

--- elasticIP/elasticIP.tf
+++ elasticIP/elasticIP.tf
@@ -5,3 +5,22 @@ provider "aws" {
 resource "aws_eip" "lb" {
   domain = "vpc"
 }
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+    name = "attribute-firewall"
+    vpc_id = aws_default_vpc.default.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4 = "${aws_eip.lb.public_ip}/32"
+  from_port = 80
+  ip_protocol = "tcp"
+  to_port = 80
+}
