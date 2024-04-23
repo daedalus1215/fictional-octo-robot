0ba8e247ed310db97ad580a8bb1d1f1b7913aa37
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-04-22 21:34:14 -0400

    adding cross-reference-attr

--- cross-reference-attributes/cross-reference-attributes.tf
+++ cross-reference-attributes/cross-reference-attributes.tf
@@ -1,3 +1,11 @@
+resource "aws_vpc" "default" {
+  cidr_block = "172.31.0.0/16"
+
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
 resource "aws_eip" "lb" {
     domain = "vpc"
 }
@@ -5,9 +13,18 @@ resource "aws_eip" "lb" {
 resource "aws_security_group" "allow_tls" {
     name = "allow_tls"
     description = "Allow TLS inbound traffic and all outbound traffic"
-    vpc_id = aws_vpc.main.id
+    vpc_id = aws_vpc.default.id
 
     tags = {
         Name = "allow_tls"
     }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "example" {
+  security_group_id = aws_security_group.allow_tls.id
+
+  cidr_ipv4   = "${aws_eip.lb.public_ip}/32"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
 }
