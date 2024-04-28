fbcb24cbf3ed2f0c488c6068c36cde22437e2a88
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-04-28 07:23:32 -0400

    do this: count-parameters

--- count-parameter/count-parameter.tf
+++ count-parameter/count-parameter.tf
@@ -4,13 +4,9 @@ provider "aws" {
   secret_key = ""
 }
 
+# Don't do this
 resource "aws_instance" "instance-1" {
     ami = "ami-23"
     instance_type = "t2.micro"
-}
-
-# Don't do this
-resource "aws_instance" "instance-2" {
-    ami = "ami-23"
-    instance_type = "t2.micro"
+    count = 2
 }
