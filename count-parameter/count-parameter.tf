c8e23b39cc7404ebd4ab1426128c550b1c083631
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-04-28 07:39:16 -0400

    conditionally set count (how many instances are created) with variable injected from .tfvars

--- count-parameter/count-parameter.tf
+++ count-parameter/count-parameter.tf
@@ -4,14 +4,12 @@ provider "aws" {
   secret_key = ""
 }
 
-variable "elb_names" {
-  type    = list(any)
-  default = ["dev-loadbalancer", "stage-loadbalancer", "prod-loadbalancer"]
+variable "istest" {
+
 }
 
-
-resource "aws_iam_user" "lb" {
-  name  = var.elb_names[count.index]
-  count = 3
-  path  = "/system/"
+resource "aws_instance" "dev" {
+  count         = var.istest == true ? 1 : 0
+  ami           = "ami-2343"
+  instance_type = "t2.micro"
 }
