9c1ae009562ace1110994b28526a0ec128996869
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-05-04 13:27:23 -0400

    replace output of arns and names with a zipmap output of the arns and names

--- splat/splat.tf
+++ splat/splat.tf
@@ -4,11 +4,6 @@ resource "aws_iam_user" "lb" {
     path = "/system/"
 }
 
-output "arns" {
-    value = aws_iam_user.lb[*].arn
+output "combined" {
+    value = zipmap(aws_iam_user.lb[*].name, aws_iam_user.lb[*].arn)
 }
-
-output "name" {
-    value = aws_iam_user.lb[*].name
-}
-
