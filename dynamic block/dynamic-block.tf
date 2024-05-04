02f0014dded23f35e3f6d14222e9e8880ef0a80b
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-05-03 22:40:03 -0400

    add egress to dynamic-block

--- dynamic block/dynamic-block.tf
+++ dynamic block/dynamic-block.tf
@@ -18,4 +18,14 @@ resource "aws_security_group" "dynamicsg" {
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
+
+  dynamic "egress" {
+    for_each = var.sg_ports
+    content {
+        from_port = egress.value
+        to_port = egress.value
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
 }
