d163c5562bd6389920c833dab9f6974bac2b05f7
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-05-05 09:36:20 -0400

    begin to migrate to modules

--- cross-reference-attributes/cross-reference-attributes.tf
+++ cross-reference-attributes/cross-reference-attributes.tf
@@ -47,6 +47,10 @@ resource "aws_instance" "my_ec2" {
   ami           = "ami-0a699202e5027c10d"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.my_subnet.id
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
 }
 
 resource "aws_ec2_tag" "example" {
